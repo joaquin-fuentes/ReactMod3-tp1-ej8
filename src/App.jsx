95d6081683f8a0e84acaa20bc9c31c48109c0b76
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-25 14:38:43 -0300

    se creo estructura principal del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+        Hola mundo
+    </div>
+  );
+};
+
+export default App;
